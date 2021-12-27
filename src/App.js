421fa5a911b205106a66d658f3ce653783d2ba33
Matthew <mattsully01@gmail.com>
2021-12-27 16:02:14 -0700

    creating dev branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import './Jumbotron.css'
+import Navbar from './components/Navbar';
+import Jumbotron from './components/Jumbotron';
 
 function App() {
   return (
     <div className="App">
+        <Navbar/>
+        <Jumbotron/>
       <header className="App-header">
+
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class Jumbotron extends Component {
+    render() {
+        return (
+            <div>
+                <div class="jumbotron jumbotron-fluid mb-0">
+                    <div class="container">
+                        <h1> Campaign Companion</h1>
+                        <p>V.1.0.0</p>
+                        <p>(Build)</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Jumbotron
